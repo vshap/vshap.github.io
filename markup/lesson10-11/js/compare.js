3f755307f77324f64d7ff57f0302f92ebc772bd4
Vitalii Shapoval <vitalii.shapoval@gmail.com>
2015-12-06 20:08:38 +0200

    Update

--- markup/lesson10-11/js/compare.js
+++ markup/lesson10-11/js/compare.js
@@ -1,23 +1,23 @@
 // Функция поиска заданного элемента в массиве
 function check_user(arr) {
-  var flag;
+    var flag;
 
-  for (var i = 0; i < user.length; i++) {
-    if(user[i] == input) {
-      flag = true; // совпадение найдено
-      break // прерываем цикл
+    for (var i = 0; i < user.length; i++) {
+        if (user[i] === input) {
+            flag = true; // совпадение найдено
+            break; // прерываем цикл
+        }
     }
-  }
-  if (flag) {
-    alert(input + ', вы успешно вошли');
-  } else {
-      alert(input + ', вы не в списке!');
-  }
+    if (flag) {
+        alert(input + ', вы успешно вошли');
+    } else {
+        alert(input + ', вы не в списке!');
+    }
 }
 
 // ---------------------------------------------
 var i = 0;
-var user = new Array(); // создаем новый массив
+var user = new Array (); // создаем новый массив
 
 do {
   user[i] = prompt('Введите имя', '');
