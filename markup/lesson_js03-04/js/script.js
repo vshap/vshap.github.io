39cc0566e1381be97b771d59a54308a968912839
Vitalii Shapoval <vitalii.shapoval@gmail.com>
2015-12-22 22:39:43 +0200

    Bootstrap replace

--- markup/lesson_js03-04/js/script.js
+++ markup/lesson_js03-04/js/script.js
@@ -1,13 +1,78 @@
-/**
- * Created by Vitalii on 07.12.2015.
- */
-var element = document.createElement('div');
+var app = {
+    createElement: function(params) {
+        var element = document.createElement(params.tagName);
 
-element.classList.add('main');
-element.innerHTML = 'dynamically created element';
+        if (params.inputType){
+            element.setAttribute('type', params.inputType);
+        }
+
+        if (params.className){
+            element.className = params.className;
+        }
+
+        if (params.content){
+            element.innerHTML = params.content;
+        }
+
+        if (params.parentElement){
+            params.parentElement.appendChild(element);
+        }
+
+        return element;
+    },
+    generateQuestions: function(questionsAmount, answersAmount){
+
+        for (var i = 0; i < questionsAmount; i++){
+
+            this.createElement({
+                tagName: 'h2',
+                content: 'Вопрос №' + (i + 1),
+                parentElement: form
+            });
+
+            for (var j = 0; j < answersAmount; j++){
+
+                var label = this.createElement({
+                    tagName: 'label',
+                    content: 'Вариант ответа №' + (i + 1),
+                    parentElement: form
+                });
+
+                var checkbox = this.createElement({
+                    tagName: 'input',
+                    inputType: 'checkbox',
+                });
+
+                label.insertAdjacentElement('afterBegin', checkbox);
+            }
+        }
+    }
+};
 
 var body = document.querySelector('body');
-body.appendChild(element);
 
-// var a = new Element('a', { 'class': 'foo', href: '/foo.html' }).update("Next page");
+app.createElement({
+    tagName: 'h1',
+    className: 'title',
+    content: 'Тест по программированию',
+    parentElement: body
+});
 
+var form = app.createElement({
+    tagName: 'form',
+    parentElement: body,
+    roleName: 'form',
+    idName: 'test',
+    actionURL: '#',
+    methodType: 'post'
+});
+
+app.generateQuestions(3, 3);
+
+app.createElement({
+    tagName: 'input',
+    inputType: 'submit',
+    className: 'check-results',
+    content: 'Проверить мои результаты',
+    parentElement: form
+});
