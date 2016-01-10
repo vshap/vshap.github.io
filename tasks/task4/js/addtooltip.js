2977fcc17e3226012a0f43e5684a3c321d10894f
Vitalii Shapoval <vitalii.shapoval@gmail.com>
2016-01-10 19:56:02 +0200

    task 4 update

--- tasks/task4/js/addtooltip.js
+++ tasks/task4/js/addtooltip.js
@@ -2,7 +2,14 @@ var elem = document.querySelectorAll('a');
 
 for (var i = 1; i <= 5; i++) {
   // show tooltip when mouse is over
-  elem[i].addEventListener('mouseover', OverIm.bind(null, 'hint' + i));
-  // hide tooltip when mouse is out of
-  elem[i].addEventListener('mouseout', OutIm);
+  if (window.addEventListener) {
+    elem[i].addEventListener('mouseover', OverIm.bind(null, 'hint' + i), false);
+    // hide tooltip when mouse is out of
+    elem[i].addEventListener('mouseout', OutIm, false);
+    } else if (window.attachEvent) { // IE8-
+        elem[i].attachEvent('onmouseover', function() {
+          return OverIm('hint' + i);
+        });
+        elem[i].attachEvent('onmouseout', OutIm);
+    }
 }
